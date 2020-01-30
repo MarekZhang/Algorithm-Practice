5fa1a55399eecc84c538b3b29e01be228a047f9e
MarkZhang <49737031+MarekZhang@users.noreply.github.com>
2020-01-30 21:36:33 +0000

    Binary Search Tree

---
+++ Chapter5-Binary-Search-tree/02-Binary-Search-Tree-Basic/src/algo/BST.java
@@ -0,0 +1,36 @@
+package algo;
+
+public class BTS<Key extends Comparable, Value>{
+
+    private class Node{
+        private Key key;
+        private Value value;
+        Node left;
+        Node right;
+
+        private Node(Key key, Value value){
+            this.key = key;
+            this.value = value;
+            left=right=null;
+        }
+    }
+
+    private Node root;
+    private int count;
+
+    public BTS(){
+        root = null;
+        count = 0;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public isEmpty(){
+        return count == 0;
+    }
+
+
+
+}
